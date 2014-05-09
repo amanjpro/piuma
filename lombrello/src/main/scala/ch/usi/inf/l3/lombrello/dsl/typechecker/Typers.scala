cc463469f033a928e4d93524980f0d18a563901a
amanjpro <amanj.sherwany@usi.ch>
2014-05-09 14:34:25 +0200

    Add a very basic template of the typechecker

---
+++ lombrello/src/main/scala/ch/usi/inf/l3/lombrello/dsl/typechecker/Typers.scala
@@ -0,0 +1,18 @@
+package ch.usi.inf.l3.lombrello.dsl.typechecker
+
+/**
+ * @author Amanj Sherwany
+ * @date 9 May 2014
+ */
+
+import ch.usi.inf.l3.lombrello
+import lombrello.dsl._
+
+trait Typers {self: Compiler =>
+
+}
+
+
+
+// TODO: You can type check the dsl using scala's reflect api
+// ru.runtimeMirror(myClassLoader).staticClass("model.Model").typeSignature.members
