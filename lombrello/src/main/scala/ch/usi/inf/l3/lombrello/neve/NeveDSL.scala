6ef9557e26883433b3de1dc5d1221e180f4b6c44
amanjpro <amanj.sherwany@usi.ch>
2014-06-03 14:06:27 +0200

    Add plugin, phase, describe and name macros.
    The way describe and name macros work is not supported by Scala macro,
    but I simply substitute method calls with variable definitions inside
    plugin and phase annotation macros.

--- lombrello/src/main/scala/ch/usi/inf/l3/lombrello/neve/NeveDSL.scala
+++ lombrello/src/main/scala/ch/usi/inf/l3/lombrello/neve/NeveDSL.scala
@@ -4,26 +4,79 @@ package ch.usi.inf.l3.lombrello.neve
 import scala.reflect.macros.whitebox.Context
 import scala.language.experimental.macros
 import scala.annotation.StaticAnnotation
+import scala.annotation.tailrec
+import scala.reflect.runtime.universe._
 
 
 object NeveDSL {
+
+
+  // Helper functions
+  private def fail(c: Context, msg: String): Nothing = {
+    c.abort(c.enclosingPosition, msg)
+  }
+
+
+  private def checkParents(c: Context)
+           (parents: List[c.universe.Tree]): Unit = {
+    // I convert them to String, because pattern matching does not work
+    // properly for Trees
+    parents.map(_.toString) match {
+      case "scala.AnyRef" :: Nil => ()
+      case _ => fail(c, "Phase classes cannot extend and/or "+
+                "mix in anything other than AnyRef\n" +
+                "       expected: List(scala.AnyRef)\n" +
+                "          found: " + parents)
+    }
+  }
+
+
+
+
+  // phase macro
   class phase extends StaticAnnotation {
-    def macroTransform(annottees: Any*): Any = macro phaseMacro.impl
+    def macroTransform(annottees: Any*): Any= macro phaseMacro.phaseImpl
   }
 
 
   object phaseMacro {
-    def impl(c: Context)(annottees: c.Expr[Any]*): c.Expr[Any] = {
+    
+    def phaseImpl(c: Context)(annottees: c.Expr[Any]*): c.Expr[Any] = {
       import c.universe._
 
       val inputs = annottees.map(_.tree).toList
       val expandee = inputs match {
         case (clazz: ClassDef) :: Nil => 
-          println(clazz.impl.parents)
-          clazz
+          
+          // Cannot extend anything except AnyRef, and you should
+          // remove it
+          checkParents(c)(clazz.impl.parents)
+          val nbody = clazz.impl.body.foldLeft(List[Tree]())((z, y) => {
+            y match {
+              // This case is to bypass the limitations of DefMacro
+              case Apply(Ident(TermName("name")), 
+                List(str @ (Literal(Constant(x: String))))) =>
+                q"val phaseName: java.lang.String = ${str}" :: z
+              case DefDef(_, n, _, _, _, _) if(n == termNames.CONSTRUCTOR) =>
+                z
+              case _ =>
+                y :: z
+            }
+          })
+          // ClassDef(clazz.mods, clazz.name, clazz.tparams, nimpl)
+
+          q"""
+          class ${clazz.name}
+          (val plgn: ch.usi.inf.l3.lombrello.transform.dsl.TransformerPlugin)
+          extends 
+          ch.usi.inf.l3.lombrello.transform.dsl.TransformerPluginComponent(
+              plgn) {
+            import global._
+            ..${nbody}
+          }
+          """
         case x => 
-          c.abort(c.enclosingPosition, 
-            "@phase can only be applied on classes")
+          fail(c, "@phase can only be applied on classes")
           EmptyTree
       }
 
@@ -33,22 +86,63 @@ object NeveDSL {
   }
 
 
-  class plugin extends StaticAnnotation {
-    def macroTransform(annottees: Any*): Any = macro pluginMacro.impl
+  // plugin macro
+  class plugin(n: Any*) extends StaticAnnotation {
+    def macroTransform(annottees: Any*): Any = macro pluginMacro.pluginImpl
   }
 
   object pluginMacro {
-    def impl(c: Context)(annottees: c.Expr[Any]*): c.Expr[Any] = {
+    def pluginImpl(c: Context)(annottees: c.Expr[Any]*): c.Expr[Any] = {
       import c.universe._
 
       val inputs = annottees.map(_.tree).toList
       val expandee = inputs match {
         case (clazz: ClassDef) :: Nil => 
-          println(clazz.impl.parents)
-          clazz
+          // Cannot extend anything except AnyRef, and you should
+          // remove it
+          checkParents(c)(clazz.impl.parents)
+
+          val params: List[Tree] = c.prefix.tree match {
+            case Apply(_, xs) =>
+              xs.map((x) => {
+                val id = Ident(TypeName(x.toString))
+                q"new ${id}(this)"
+              })
+            case _ =>
+              fail(c, "@plugin should define at least one plugin")
+              Nil
+          }
+
+
+          val components = q"""val pluginComponents: 
+           List[
+           ch.usi.inf.l3.lombrello.transform.dsl.TransformerPluginComponent] = 
+                 List(..${params})"""
+
+          val tbody = components :: clazz.impl.body
+          val nbody = tbody.foldLeft(List[Tree]())((z, y) => {
+
+            y match {
+              // This case is to bypass the limitations of DefMacro
+              case Apply(Ident(TermName("describe")), 
+                List(str @ (Literal(Constant(x: String))))) =>
+                q"override val description: java.lang.String = ${str}" :: z
+              case DefDef(_, n, _, _, _, _) if(n == termNames.CONSTRUCTOR) =>
+                z
+              case _ =>
+                y :: z
+            }
+          })
+          q"""
+            class ${clazz.name}(override val global: 
+                      scala.tools.nsc.Global) extends 
+            ch.usi.inf.l3.lombrello.transform.dsl.TransformerPlugin(global) {
+              ..${nbody}
+            }
+          """
+          // ClassDef(clazz.mods, clazz.name, clazz.tparams, nimpl)
         case x => 
-          c.abort(c.enclosingPosition, 
-            "@plugin can only be applied on classes")
+          fail(c, "@plugin can only be applied on classes")
           EmptyTree
       }
 
