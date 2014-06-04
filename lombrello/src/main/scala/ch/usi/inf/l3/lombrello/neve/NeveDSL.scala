7771ac7ad7d6a80ccbade28c02323bf32461e2f9
amanjpro <amanj.sherwany@usi.ch>
2014-06-04 17:21:30 +0200

    Add checker macro, and refactor the code.

--- lombrello/src/main/scala/ch/usi/inf/l3/lombrello/neve/NeveDSL.scala
+++ lombrello/src/main/scala/ch/usi/inf/l3/lombrello/neve/NeveDSL.scala
@@ -11,24 +11,7 @@ import scala.reflect.runtime.universe._
 object NeveDSL {
 
 
-  // Helper functions
-  private def fail(c: Context, msg: String): Nothing = {
-    c.abort(c.enclosingPosition, msg)
-  }
-
-
-  private def checkParents(c: Context)
-           (parents: List[c.universe.Tree]): Unit = {
-    // I convert them to String, because pattern matching does not work
-    // properly for Trees
-    parents.map(_.toString) match {
-      case "scala.AnyRef" :: Nil => ()
-      case _ => fail(c, "Phase classes cannot extend and/or "+
-                "mix in anything other than AnyRef\n" +
-                "       expected: List(scala.AnyRef)\n" +
-                "          found: " + parents)
-    }
-  }
+  
 
 
 
@@ -43,10 +26,47 @@ object NeveDSL {
     def macroTransform(annottees: Any*): Any = macro Macros.pluginImpl
   }
 
-
+  // checker macro
+  class checker extends StaticAnnotation {
+    def macroTransform(annottees: Any*): Any= macro Macros.checkerImpl
+  }
   
   object Macros {
     
+    def checkerImpl(c: Context)(annottees: c.Expr[Any]*): c.Expr[Any] = {
+      import c.universe._
+      val inputs = annottees.map(_.tree).toList
+      val expandee = inputs match {
+        case (clazz: ClassDef) :: Nil => 
+          
+          // Cannot extend anything except AnyRef, and you should
+          // remove it
+          checkParents(c)(clazz.impl.parents)
+
+
+          val nbody = generateBody(c)(clazz.impl.body, true)
+                    
+          q"""
+          class ${clazz.name}
+          (val plgn: ch.usi.inf.l3.lombrello.plugin.LombrelloPlugin)
+          extends 
+          ch.usi.inf.l3.lombrello.plugin.CheckerPluginComponent(
+              plgn) {
+            import global._
+            import plgn._
+            import plgn.utilities._
+            ..${nbody}
+          }
+          """
+        case x => 
+          fail(c, "@checker can only be applied on classes")
+          EmptyTree
+      }
+
+
+      c.Expr[Any](expandee)
+    }
+
     def phaseImpl(c: Context)(annottees: c.Expr[Any]*): c.Expr[Any] = {
       import c.universe._
 
@@ -58,82 +78,18 @@ object NeveDSL {
           // remove it
           checkParents(c)(clazz.impl.parents)
 
-          val nameTree: Tree = c.prefix.tree match {
-            case Apply(_, x :: Nil) =>
-                x match {
-                  case str @ Literal(Constant(_)) =>
-                    q"val phaseName: java.lang.String = ${str}"
-                  case _ =>
-                    fail(c, "@phase should provide a name of type String")
-                    EmptyTree
-                }
-            case _ =>
-              fail(c, "@phase should provide a single name")
-              EmptyTree
-          }
 
-          val tbody1 = nameTree :: clazz.impl.body
-
-          val runsRightAfter = tbody1.foldLeft(None: Option[Tree])((z, y) => {
-            y match {
-              case Apply(Ident(TermName("rightAfter")), List(x)) =>
-                Some(x)
-              case _ => z
-            }
-          })
-
-
-
-
-
-          // If rightAfter is declared, after won't be necessary
-          val hasRunsAfter = tbody1.foldLeft(false)((z, y) => {
-            y match {
-              case Apply(Ident(TermName("after")), List(x)) =>
-                true
-              case _ => z
-            }
-          })
-
-
-          val tbody2 = if(runsRightAfter != None && !hasRunsAfter) {
-                         q"after(List(${runsRightAfter.get}))" :: tbody1
-                       } else {
-                         tbody1
-                       }
-
-          val nbody = tbody2.foldLeft(List[Tree]())((z, y) => {
-            y match {
-              case DefDef(_, TermName("transform"), Nil,
-                  List(List(x)), tpt, rhs) =>
-                q"""
-                final override def newTransformer(unit: CompilationUnit):
-                    Transformer = new TransformerComponent(unit) {
-                  final override def transform(${x.name}: ${x.tpt}): 
-                        ${tpt} = ${rhs}
-                }
-                """ :: z
-              case Apply(Ident(TermName("rightAfter")), List(x)) =>
-                q"override val runsRightAfter: Option[String] = Some(${x})" :: z
-              case Apply(Ident(TermName("after")), List(x)) =>
-                q"val runsAfter: List[String] = ${x}" :: z
-              case Apply(Ident(TermName("before")), List(x)) =>
-                q"override val runsBefore: List[String] = ${x}" :: z
-              case DefDef(_, n, _, _, _, _) if(n == termNames.CONSTRUCTOR) =>
-                z
-              case _ =>
-                y :: z
-            }
-          })
-
+          val nbody = generateBody(c)(clazz.impl.body, false)
+                    
           q"""
           class ${clazz.name}
-          (val plgn: ch.usi.inf.l3.lombrello.transform.dsl.TransformerPlugin)
+          (val plgn: ch.usi.inf.l3.lombrello.plugin.LombrelloPlugin)
           extends 
-          ch.usi.inf.l3.lombrello.transform.dsl.TransformerPluginComponent(
+          ch.usi.inf.l3.lombrello.plugin.TransformerPluginComponent(
               plgn) {
             import global._
             import plgn._
+            import plgn.utilities._
             ..${nbody}
           }
           """
@@ -170,8 +126,7 @@ object NeveDSL {
 
 
           val components = q"""val pluginComponents: 
-           List[
-           ch.usi.inf.l3.lombrello.transform.dsl.TransformerPluginComponent] = 
+           List[scala.tools.nsc.plugins.PluginComponent] = 
                  List(..${params})"""
 
           val tbody = components :: clazz.impl.body
@@ -191,7 +146,7 @@ object NeveDSL {
           q"""
             class ${clazz.name}(override val global: 
                       scala.tools.nsc.Global) extends 
-            ch.usi.inf.l3.lombrello.transform.dsl.TransformerPlugin(global) {
+            ch.usi.inf.l3.lombrello.plugin.LombrelloPlugin(global) {
               ..${nbody}
             }
           """
@@ -204,11 +159,136 @@ object NeveDSL {
 
       c.Expr[Any](expandee)
     }
+  }
 
-    
+  // Helper functions
+  private def fail(c: Context, msg: String): Nothing = {
+    c.abort(c.enclosingPosition, msg)
   }
 
 
-  
+  private def checkParents(c: Context)
+           (parents: List[c.universe.Tree]): Unit = {
+    // I convert them to String, because pattern matching does not work
+    // properly for Trees
+    parents.map(_.toString) match {
+      case "scala.AnyRef" :: Nil => ()
+      case _ => fail(c, "Phase classes cannot extend and/or "+
+                "mix in anything other than AnyRef\n" +
+                "       expected: List(scala.AnyRef)\n" +
+                "          found: " + parents)
+    }
+  }
+
+
+  private def getNameTree(c: Context, caller: String): c.universe.Tree = {
+    import c.universe._
+    c.prefix.tree match {
+      case Apply(_, x :: Nil) =>
+        x match {
+          case str @ Literal(Constant(_)) =>
+            q"val phaseName: java.lang.String = ${str}"
+          case _ =>
+            fail(c, s"${caller} should provide a name of type String")
+            EmptyTree
+        }
+      case _ =>
+        fail(c, s"${caller} should provide a single name")
+        EmptyTree
+    }
+  }
+
+  private def extractRightAfter(c: Context)
+            (body: List[c.universe.Tree]): Option[c.universe.Tree] = {
+    import c.universe._
+    body.foldLeft(None: Option[Tree])((z, y) => {
+      y match {
+        case Apply(Ident(TermName("rightAfter")), List(x)) =>
+          Some(x)
+        case _ => z
+      }
+    })
+  }
+
+
+  private def doesHaveAfter(c: Context)
+        (body: List[c.universe.Tree]): Boolean = {
+    import c.universe._
+    body.foldLeft(false)((z, y) => {
+      y match {
+        case Apply(Ident(TermName("after")), List(x)) =>
+          true
+        case _ => z
+      }
+    })
+
+  }
+
+
+  private def bodyMacros(c: Context)(body: List[c.universe.Tree],
+        isChecker: Boolean): List[c.universe.Tree] = {
+    import c.universe._
+    body.foldLeft(List[Tree]())((z, y) => {
+      y match {
+        case DefDef(_, TermName("check"), Nil,
+          List(List(x)), tpt, rhs) if isChecker =>
+          q"""
+          final def newPhase(_prev: scala.tools.nsc.Phase): StdPhase = 
+            new CheckerComponent(_prev) {
+              final def apply(${x.name}: ${x.tpt}): ${tpt} = ${rhs}
+            }
+          """ :: z
+        case DefDef(_, TermName("transform"), Nil,
+          List(List(x)), tpt, rhs) if !isChecker =>
+          q"""
+          final override def newTransformer(unit: CompilationUnit):
+                      Transformer = new TransformerComponent(unit) {
+              final override def transform(${x.name}: ${x.tpt}): ${tpt} = ${rhs}
+            }
+          """ :: z
+        case Apply(Ident(TermName("rightAfter")), List(x)) =>
+          q"override val runsRightAfter: Option[String] = Some(${x})" :: z
+        case Apply(Ident(TermName("after")), List(x)) =>
+          q"val runsAfter: List[String] = ${x}" :: z
+        case Apply(Ident(TermName("before")), List(x)) =>
+          q"override val runsBefore: List[String] = ${x}" :: z
+        case DefDef(_, n, _, _, _, _) if(n == termNames.CONSTRUCTOR) =>
+          z
+        case _ =>
+          y :: z
+      }
+    })
+  }
+
+
+  private def generateBody(c: Context)(body: List[c.universe.Tree],
+        isChecker: Boolean): List[c.universe.Tree] = {
+
+    import c.universe._
+    val kindStr = isChecker match {
+      case false => "@phase"
+      case true => "@checker" 
+    }
+    val nameTree: Tree = getNameTree(c, kindStr)
+
+    val tbody1 = nameTree :: body
+
+    val runsRightAfter = extractRightAfter(c)(tbody1)
+
+
+
+
+    // If "rightAfter" is declared, "after" won't be necessary
+    val hasRunsAfter = doesHaveAfter(c)(tbody1)
+
+    val tbody2: List[c.universe.Tree] = 
+      if(runsRightAfter != None && !hasRunsAfter) {
+        q"after(List(${runsRightAfter.get}))" :: tbody1
+      } else {
+        tbody1
+      }
+
+      bodyMacros(c)(tbody2, isChecker)
+  }
 }
 
