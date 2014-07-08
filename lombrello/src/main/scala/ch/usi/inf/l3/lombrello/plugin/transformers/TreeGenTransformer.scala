91a530a12c8d8821db28cd93cc4561735fe8c943
amanjpro <amanj.sherwany@usi.ch>
2014-07-08 23:42:54 +0200

    Add Literal and Apply factory methods.

--- lombrello/src/main/scala/ch/usi/inf/l3/lombrello/plugin/transformers/TreeGenTransformer.scala
+++ lombrello/src/main/scala/ch/usi/inf/l3/lombrello/plugin/transformers/TreeGenTransformer.scala
@@ -16,6 +16,69 @@ trait TreeGenTransformerCake {
 
     import renamer.plgn.global._
     
+
+    /**
+      * Creates a literal contant.
+      *
+      * @param const contant value of the literal
+      * 
+      * @return a Literal contant of the given "cosnt"
+      */
+    def mkLiteral[T <: AnyVal](const: AnyVal): Literal = {
+      Literal(Constant(const))
+    }
+
+    /**
+      * Creates a literal contant.
+      *
+      * @param const contant value of the literal
+      * 
+      * @return a Literal contant of the given "cosnt"
+      */
+    def mkLiteral(const: String): Literal = {
+      Literal(Constant(const))
+    }
+
+    /**
+      * Creates a literal unit constant.
+      *
+      * @return a Literal contant of the "Unit"
+      */
+    def mkUnitLiteral: Literal = {
+      Literal(Constant(()))
+    }
+
+    /**
+      * Creates a tree for method/function appliation (aka method call)
+      *
+      * @param fun the function/method to be called
+      * @param args a list of arguments for the function/method
+      *
+      * @return a tree of the function/method appliation
+      */
+    def mkApply(fun: Tree, args: List[Tree]): Apply = {
+      localTyper.typed { Apply(fun, args) }.asInstanceOf[Apply]
+    }
+
+    /**
+      * Creates a tree for a generic method/function appliation 
+      * (aka method call)
+      *
+      * @param fun the function/method to be called
+      * @param targs a list of type arguments
+      * @param args a list of arguments for the function/method
+      *
+      * @return a tree of the function/method appliation
+      */
+    def mkApply(fun: Tree, targs: List[Tree], args: List[Tree]): Apply = {
+      targs match {
+        case Nil => mkApply(fun, args)
+        case _ =>
+          val tpapply = TypeApply(fun, targs)
+          mkApply(tpapply, args)
+
+      }
+    }
     
 // ------ Generating Variables -----------------------------------------------------
     // Issue#4 is an open bug about default params, make sure to fix it
@@ -86,6 +149,136 @@ trait TreeGenTransformerCake {
       val rhs = Select(This(owner), vsym.name) 
       val gtr = typer.atOwner(owner) typed {DefDef(gtrSym, rhs)}
       gtr.asInstanceOf[DefDef]
+    }
+
+
+
+
+
+    /**
+      * Returns a well-typed DefDef (method).
+      *
+      * @param name the name of the method
+      * @param tparams a list of the type parameters of the method
+      * @param vaparamss a list of list of parameters of the method
+      * @param tpt the tpt (type tree) of the method
+      * @param rhs the right-hand-side of the method
+      * @param symbol the symbol of the method
+      *
+      * @return a well typed method
+      */
+    def mkDefDef(name: TermName, tparams: List[TypeDef],
+                  vparamss: List[List[ValDef]], tpt: Tree,
+                  rhs: Tree, symbol: Symbol): DefDef = {
+      localTyper.typed { 
+        DefDef(Modifiers(symbol.flags),
+            name, 
+            tparams, vparamss, tpt,
+            rhs).setSymbol(symbol) 
+      }.asInstanceOf[DefDef]
+    }
+     
+    /**
+      * Returns a well-typed DefDef (method).
+      *
+      * @param rhs the right-hand-side of the method
+      * @param symbol the symbol of the method
+      *
+      * @return a well typed method
+      */
+
+    def mkDefDef(rhs: Tree, symbol: Symbol): DefDef = {
+      localTyper.typed {
+        DefDef(symbol, rhs)
+      }.asInstanceOf[DefDef]
+    }
+
+    /**
+      * Returns a well-typed DefDef (method).
+      *
+      * @param flags the flags of the method
+      * @param name the name of the method
+      * @param tparams a list of the type parameters of the method
+      * @param vaparamss a list of list of parameters of the method
+      * @param ret the return type of the method
+      * @param rhs the right-hand-side of the method
+      * @param owner the owner of the method
+      *
+      * @return a well typed method
+      */
+    def mkDefDef(flags: Long, name: TermName, tparams: List[TypeDef],
+                  vparamss: List[List[ValDef]], ret: Type,
+                  rhs: Tree, owner: Symbol): DefDef = {
+      val symbol = owner.newMethodSymbol(
+            name, owner.pos.focus, flags)
+
+      val paramSyms = vparamss.flatten.map(_.symbol)
+      val tparamSyms = tparams.map(_.symbol)
+
+      val tpe = tparams match {
+        case Nil => MethodType(paramSyms, ret)
+        case _ => PolyType(tparamSyms, MethodType(paramSyms, ret))
+      }
+
+      symbol.setInfoAndEnter(tpe)
+
+      mkDefDef(rhs, symbol)
+    }
+
+
+    
+    /**
+      * Converts a method to a function.
+      *
+      * @param defdef the method to be converted to function
+      * @owner the owner of the resulted function
+      * @apply a call to the method that needs to be converted
+      * 
+      * @return returns a function that represented the given method
+      */
+    def def2Function(defdef: DefDef, owner: Symbol, apply: Apply): Function = {
+
+      val outerClass = defdef.symbol.enclClass
+      val closureSelect = owner.isClass match {
+        case true =>
+          Select(This(outerClass), defdef.symbol)
+        case false =>
+          Ident(defdef.symbol)
+      }
+
+      val closureSym = owner.newTermSymbol(
+          TermName(tpnme.ANON_FUN_NAME.toString), 
+              owner.pos.focus, Flags.SYNTHETIC)
+
+
+
+      val closureParamSyms = defdef.vparamss.flatten.map((p) => {
+        val temp = closureSym.newValueParameter(
+                    TermName(p.symbol.name.toString), 
+                    closureSym.pos.focus, p.symbol.flags)
+        temp.info = p.symbol.info
+        temp
+      })
+
+
+      val closureParams = closureParamSyms.map((x) => {
+        ValDef(x, EmptyTree)
+      })
+
+      val closureArgs = closureParamSyms.map((x) => Ident(x))
+
+      val closureTpe = TypeRef(outerClass.info, closureSym, 
+              closureParamSyms.map(_.info) ++ List(defdef.tpt.tpe))
+
+      closureSym.setInfo(closureTpe)
+
+      val closureRhs = apply.fun match {
+        case TypeApply(_, targs) => 
+          Apply(TypeApply(closureSelect, targs), closureArgs)
+        case _ => Apply(closureSelect, closureArgs)
+      }
+
+      Function(closureParams, closureRhs).setSymbol(closureSym)
     }
   }
 }
